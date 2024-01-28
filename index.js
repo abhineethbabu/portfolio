d5672aa232ce38b47a2ba2bbad89f93164ba8ffe
abhineethbabu <abhianb1@gmail.com>
2024-01-28 20:18:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const soundCloud = document.querySelector('.sound-cloud');
+const off = document.querySelector('#off');
+const on = document.querySelector('#on');
+const myAudio = document.querySelector('#myAudio');
+
+window.onload = function () {
+    Particles.init({
+      selector: ".background"
+    });
+  };
+var particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: ["#faebd7", "#03dac6", "#ff0266"],
+        maxParticles: 43,
+        connectParticles: false
+      }
+    }
+  ]
+});
+
+off.addEventListener('click', () => soundTrack('off'));
+on.addEventListener('click', () => soundTrack('on'));
+
+const soundTrack = (soundState) => {
+    if(soundState === 'off'){
+        on.style.display = 'block';
+        off.style.display = 'none';
+        soundCloud.style.color = "#08fdd8";
+        myAudio.play();
+    }
+    else if(soundState === 'on'){
+        on.style.display = 'none';
+        off.style.display = 'block';
+        soundCloud.style.color = "#f50057";
+        myAudio.pause();
+    }
+}
+
+// Play music functionality
+
+const btnBars = document.querySelector('.bars');
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
+
+btnBars.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
+    }
+}
+
+$(document).ready(function () {
+  if (
+    !$('#myCanvas').tagcanvas(
+      {
+        textColour: '#08fdd8',
+        outlineColour: 'transparent',
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weight: true,
+      },
+      'tags',
+    )
+  ) {
+    // something went wrong hide the canvas container,
+    $('#myCanvasContainer')
+  }
+})
+
